27075acfea500104c572755d8aa043b100acd676
Orlando Rodriguez Franqui <franqui335@gmail.com>
2026-04-05 21:23:53 -0400

    First Python project

---
+++ first_program.py
@@ -0,0 +1,18 @@
+# first_project.py
+# This is my first Phyton mini project 
+# It prints my informations and practices loop
+
+name = "Orlando"
+age = 24 
+
+# Print basic information 
+print ("Hello, my name is", name)
+print ("I am", age, "years old")
+print ( "I am learning programming")
+
+# Repeat a message 3 times using loop
+for i in range(3) :
+        print( "I am practicing Python and English")
+
+
+

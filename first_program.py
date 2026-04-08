9ec14a4b0609a378a2868a8c21e1004336134a7b
Orlando Rodriguez Franqui <franqui335@gmail.com>
2026-04-07 23:26:30 -0400

    Add files via upload
    Add loop and user interaction

--- first_program.py
+++ first_program.py
@@ -1,10 +1,24 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+while True:
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
 
-print(f"hello {name}, you are {age} years old.")
+    print (f"Hello {name}, you are {age}, years old.")
 
-if age >= 18:
-    print ("You are an adult")
-else :
-    print("You are young")
+    if age >=18:
+        print("You are an adult")
+    else:
+        print("You are young")
+    
+    again = input("Do you want to continue? (yes or no):")
+
+    if again.lower() == "no":
+       print("Goodbye!")
+       break
+        
+
+
+
+
+    
+
 
